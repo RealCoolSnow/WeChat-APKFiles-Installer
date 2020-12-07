48b729753fd75c8104e6c1407bb5fb995f1f9f37
CoolSnow <coolsnow2020@gmail.com>
2020-12-07 15:49:43 +0800

    update

--- app/src/main/java/app/wxapkfiles/Application.java
+++ app/src/main/java/app/wxapkfiles/Application.java
@@ -4,12 +4,14 @@ package app.wxapkfiles;
 import com.orhanobut.logger.Logger;
 
 import app.logger.LoggerConfig;
+import app.statistics.umeng.Umeng;
 
 public class Application extends android.app.Application {
     @Override
     public void onCreate() {
         super.onCreate();
         LoggerConfig.init(this, Constants.LOGGER_TAG, Constants.DEBUG);
+        Umeng.init(this, "5fcddc99bed37e4506c45f31", "", Umeng.DEVICE_TYPE_PHONE);
         Logger.i("app created");
     }
 }
